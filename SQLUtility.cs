3beef067732692e5dde8a9daa7aea9c3aa5324f1
menuchabern <menucha9021@gmail.com>
2025-05-26 20:47:47 -0400

    session 15 work

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Diagnostics;
 using System.Data;
 
 namespace CPUFramework
@@ -9,16 +10,44 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(string sqlstatement)
         {
+            Debug.Print(sqlstatement);
             DataTable dt = new();
             SqlConnection conn = new();
             conn.ConnectionString = ConnectionString;
             conn.Open();
             var cmd = new SqlCommand();
-            cmd.CommandText = sqlstatement;
             cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+
+            SetAllColumnsAllowNull(dt);
+
             return dt;
+        }
+
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach(DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
+
+        public static void DebugPrintDataTable(DataTable dt) 
+        {
+            foreach(DataRow r in dt.Rows)
+            {
+                foreach(DataColumn c in dt.Columns)
+                {
+                    Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
+                }
+            }
         }
     }
 }
