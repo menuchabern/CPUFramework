98b5f66941941078fd8def589443fa0571c89656
menuchabern <menucha9021@gmail.com>
2025-11-30 13:43:13 -0500

    session 2.40 work

---
+++ bizObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace CPUFramework
+{
+    public class bizObject
+    {
+        string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = ""; string _primarykeyname = ""; string _primarykeyparamname = "";
+        DataTable _datatable = new();
+        public bizObject(string tablename)
+        {
+            _tablename = tablename;
+            _getsproc = tablename + "Get";
+            _updatesproc = tablename + "Update";
+            _deletesproc = tablename + "Delete";
+            _primarykeyname = tablename + "Id";
+            _primarykeyparamname = "@" + _primarykeyname;
+        }
+
+        public DataTable Load(int primarykeyvalue)
+        {
+            DataTable dt = new();
+            SqlCommand cmd = SQLUtility.GetSQLCommand(_getsproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
+            dt = SQLUtility.GetDataTable(cmd);
+            _datatable = dt;
+            return dt;
+        }
+
+        public void Delete(DataTable datatable)
+        {
+            int id = (int)datatable.Rows[0][_primarykeyname];
+            SqlCommand cmd = SQLUtility.GetSQLCommand(_deletesproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, id);
+            SQLUtility.ExecuteSQL(cmd);
+        }
+
+        public void Save(DataTable datatable)
+        {
+            if (datatable.Rows.Count == 0)
+            {
+                throw new Exception($"Cannot call {_tablename} save method because there are no rows in the table");
+            }
+
+            DataRow r = datatable.Rows[0];
+            SQLUtility.SaveDataRow(r, _updatesproc);
+        }
+    }
+}
