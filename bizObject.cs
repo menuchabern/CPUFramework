4f3d362fff9c60824c613c44af976e4ad0ac25f5
menuchabern <menucha9021@gmail.com>
2025-12-03 19:10:21 -0500

    finished session 41 work

--- bizObject.cs
+++ bizObject.cs
@@ -8,7 +8,7 @@ namespace CPUFramework
 {
     public class bizObject : INotifyPropertyChanged
     {
-        string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = ""; string _primarykeyname = ""; string _primarykeyparamname = "";
+        string _typename = ""; string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = ""; string _primarykeyname = ""; string _primarykeyparamname = "";
         DataTable _datatable = new();
         List<PropertyInfo> properties = new();
 
@@ -17,7 +17,8 @@ namespace CPUFramework
         public bizObject()
         {
             Type t = this.GetType();
-            _tablename = this.GetType().Name;
+            _typename = t.Name;
+            _tablename = _typename;
             if (_tablename.ToLower().StartsWith("biz"))
             {
                 _tablename = _tablename.Substring(3);
@@ -136,7 +137,15 @@ namespace CPUFramework
                 {
                     value = null;
                 }
-                prop.SetValue(this, value);
+                try
+                {
+                    prop.SetValue(this, value);
+                }
+                catch (Exception ex)
+                {
+                    string message = $"{_typename}.{prop.Name} is being set to {value?.ToString()} and that is the wrong data type. {ex.Message}";
+                    throw new CPUDevException(message, ex);
+                }
             }
         }
 
